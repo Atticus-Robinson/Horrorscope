57e5b16cee3b70df93a8101d1e4164939ec759ad
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-19 14:29:19 -0500

    adds initial file structure and installs dependencies

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+// establish session rules
+const sess = {
+  secret: "secretSquirrel",
+  cookie: {
+    // end session on idle for 10 minutes
+    expires: 10 * 60 * 1000,
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
