c74a6c4cf7a4e05b988b8cd9cc6fa4415d2fde08
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-30 14:27:08 -0500

    updates daily reading to include data and fixes paths

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,11 +1,11 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector('#email1').value.trim();
-  const password = document.querySelector('#pw1').value.trim();
+  const email = document.querySelector('.email1').value.trim();
+  const password = document.querySelector('.pw1').value.trim();
 
-console.log(email)
-console.log(password)
+  console.log(email);
+  console.log(password);
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
@@ -25,30 +25,6 @@ console.log(password)
   }
 }
 
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-      document.location.replace('daily-reading.html');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-
-// document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
