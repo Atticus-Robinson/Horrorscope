3579012f679bc23d49933837f727d8e8c2d09f18
TennCreekBridges <bridgettvalet@gmail.com>
2022-04-02 15:06:19 -0500

    wrangling cards and trying to generate random card on homepage2

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -5,7 +5,7 @@ async function signupFormHandler(event) {
   const password = document.querySelector('#password-signup').value.trim();
   const birthday = document.querySelector('#birthday-signup').value.trim();
 
-  if (email && password) {
+  if (email && password && birthday) {
     const response = await fetch('/api/users', {
       method: 'post',
       body: JSON.stringify({
@@ -23,6 +23,8 @@ async function signupFormHandler(event) {
     }
   }
 }
+
+console.log(email)
 
 document
   .querySelector('.signup-form')
