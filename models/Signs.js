0255dd72824e0ab1e174b47293c439db30185dc0
TennCreekBridges <bridgettvalet@gmail.com>
2022-04-03 21:41:10 -0500

    updates signs model daily-reading and homepage2

--- models/Signs.js
+++ models/Signs.js
@@ -4,30 +4,30 @@ const sequelize = require('../config/connection');
 class Signs extends Model {}
 
 Signs.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-          },
-          sign_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          sign_dates: {
-              type: DataTypes.STRING,
-              allowNull: false,
-          },
-          user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-              model: 'user',
-              key: 'id'
-            }
-          }
-        },
-{
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    sign_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    sign_image: {
+      type: DataTypes.BLOB,
+      allowNull: false
+    },
+    birthday: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'birthday',
+        key: 'birthday'
+      }
+    }
+  },
+  {
     sequelize,
     freezeTableName: true,
     underscored: true,
@@ -36,4 +36,3 @@ Signs.init(
 );
 
 module.exports = Signs;
-
